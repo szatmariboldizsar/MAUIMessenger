8931cb5d2c8a0c9291e39106aa4e83fe319f3490
Szatmári Boldizsár <szatmariboldizsar@gmail.com>
2026-03-29 17:13:29 +0200

    Set up default window size and position

--- messaging-assignment/App.xaml.cs
+++ messaging-assignment/App.xaml.cs
@@ -11,7 +11,23 @@ namespace messaging_assignment
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            var window = new Window(new AppShell());
+
+            int windowWidth = 800;
+            int windowHeight = 600;
+
+            window.Width = windowWidth;
+            window.Height = windowHeight;
+
+            window.MinimumWidth = 400;
+            window.MinimumHeight = 300;
+
+            var displayInfo = DeviceDisplay.Current.MainDisplayInfo;
+
+            window.X = (displayInfo.Width / displayInfo.Density - windowWidth) / 2;
+            window.Y = (displayInfo.Height / displayInfo.Density - windowHeight) / 2;
+
+            return window;
         }
     }
 }

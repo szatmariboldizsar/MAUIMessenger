fa8ac3ecbb695163a2bcd3825e07d2eb9a3dd5d7
Szatmári Boldizsár <szatmariboldizsar@gmail.com>
2026-04-08 13:22:27 +0200

    Null check simplified

--- messaging-assignment/Controls/NullToBooleanConverter.cs
+++ messaging-assignment/Controls/NullToBooleanConverter.cs
@@ -9,7 +9,7 @@ namespace messaging_assignment.Controls
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value != null ? true : false;
+            return value is not null;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
